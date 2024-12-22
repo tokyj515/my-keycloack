b66848c6fe3c70e2cc5187f6b62280a44611d253
YuJin Kwon <tokyj515@gmail.com>
2024-12-22 16:26:09 +0900

    Initial commit

---
+++ src/test/java/com/example/mykeycloack/MyKeycloackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mykeycloack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyKeycloackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
